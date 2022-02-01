558590250f4bb6c5b8a862e23878d7c102669123
Roha-Lee <rohagru@gmail.com>
2022-02-02 00:54:59 +0900

    file read, variable 학습

---
+++ nodejs_lifecoding/syntax/variable.js
@@ -0,0 +1,12 @@
+let a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+let name = 'egoing'
+let letter = 'Dear ' + name + '\n\
+My name is Roha. ';
+console.log(letter);
+
+let letterTemplateLiteral = `Dear ${name}
+My name is Roha. Nice to meet you.`;
+console.log(letterTemplateLiteral);
