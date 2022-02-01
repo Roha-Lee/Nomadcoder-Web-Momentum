0f681f66124f0f211c53ac45bf70e48d538f30a7
Roha-Lee <rohagru@gmail.com>
2022-02-01 17:13:40 +0900

    [modify] 공백 strip할수 있도록 수정

--- momentum_app/js/greetings.js
+++ momentum_app/js/greetings.js
@@ -15,7 +15,7 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}. Nice to meet you!`;
+    greeting.innerText = `Hello ${username.trim()}. Nice to meet you!`;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
