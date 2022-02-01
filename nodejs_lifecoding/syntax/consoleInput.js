1acd8993a60e100510453edbb2da6bf599c99bc2
Roha-Lee <rohagru@gmail.com>
2022-02-02 01:15:52 +0900

    [modify] body content부분 파일에서 불러오도록 수정

---
+++ nodejs_lifecoding/syntax/consoleInput.js
@@ -0,0 +1,10 @@
+const args = process.argv;
+console.log(args);
+//node consoleInput.js Roha Gru -v 
+// [
+    // '/Users/jongholee/.nvm/versions/node/v12.13.1/bin/node',
+    // '/Users/jongholee/studyspace/FrontEnd/NomadCoder/Nomadcoder-Web-Study/nodejs_lifecoding/syntax/consoleInput.js',
+    // 'Roha',
+    // 'Gru',
+    // '-v'
+// ]
