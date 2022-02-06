4a35325d92675f35be368db528738f8b9dd25abd
Roha-Lee <rohagru@gmail.com>
2022-02-06 21:46:31 +0900

    part1

---
+++ backend_study/call_stack.js
@@ -0,0 +1,15 @@
+function first() {
+    second();
+    console.log('1st');
+}
+
+function second() {
+    third();
+    console.log('2nd');
+}
+
+function third() {
+    console.log('3rd');
+}
+
+first();
