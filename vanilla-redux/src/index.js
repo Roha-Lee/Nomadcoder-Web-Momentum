f9c9337a13fd4ec0efb6441649d1d481d02d3473
Roha-Lee <rohagru@gmail.com>
2022-04-03 14:15:49 +0900

    feat: subscribe이용하여 변화 감지

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -7,15 +7,16 @@ const number = document.querySelector('span');
 // reducer가 return 하는 값이 data가 된다. 
 // 두번째 매개변수인 action을 통해 값을 바꾸는 작업을 한다. 
 // store.dispatch를 이용해서 action을 보낼 수 있다.
+const ADD = "ADD";
+const SUBTRACT = "SUBTRACT";
 const countModifier = (count = 0, action) => {
-  if(action.type === "ADD") {
-    return count + 1;
-  } 
-  else if(action.type === "SUBTRACT") {
-    return count - 1;
-  } 
-  else {
-    return count;
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case SUBTRACT:
+      return count - 1;
+    default:
+      return count;
   }
 }
 
@@ -24,14 +25,15 @@ const onChange = () => {
 }
 
 const countStore = createStore(countModifier);
+// subscribe: 변화가 일어남을 감지한다. 
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({type: 'ADD'});
+  countStore.dispatch({type: ADD});
 }
 
 const handleSubtract = () => {
-  countStore.dispatch({type: 'SUBTRACT'})
+  countStore.dispatch({type: SUBTRACT})
 }
 // dispatch를 부르면 redux가 countModifier(currentState, {type: "ADD"})를 호출한다. 
 add.addEventListener('click', handleAdd);
