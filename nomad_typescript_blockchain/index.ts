cf466f88221a317ebb846f75a4dd3d0d39b812be
Roha-Lee <rohagru@gmail.com>
2022-04-21 18:28:28 +0900

    class 연습 with typescript

--- nomad_typescript_blockchain/index.ts
+++ nomad_typescript_blockchain/index.ts
@@ -1,1 +1,49 @@
-console.log('hello');
+type Words = {
+    [key: string]: string
+}
+
+class Dict {
+    private words: Words;
+    constructor() {
+        this.words = {};
+    }
+    
+    add(word: Word) {
+        if (this.words[word.getTerm()] === undefined) {
+            this.words[word.getTerm()] = word.getDef();
+        }
+    }
+
+    def(term: string) {
+        return this.words[term];
+    }
+
+    del(term: string) {
+        delete this.words[term];
+    }
+
+    modify(word: Word) {
+        if (this.words[word.getTerm()] !== undefined) {
+            this.words[word.getTerm()] = word.getDef();
+        }
+    }
+}
+
+class Word {
+    constructor(
+        private term: string, 
+        private def: string
+    ) {}
+
+    modify(def: string) {
+        this.def = def;
+    }
+
+    getTerm() {
+        return this.term;
+    }
+
+    getDef() {
+        return this.def;
+    }
+}
